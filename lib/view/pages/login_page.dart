c2c81132f50ce46f0347b3a1266a7ae61ea3b47b
Gsdagustavo <gugadanielalvez@gmail.com>
2025-04-19 18:30:48 -0300

    added a login button and a forgot password button

--- lib/view/pages/login_page.dart
+++ lib/view/pages/login_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:login_screen/view/schemes/app_colorscheme.dart';
 
-import '../util/animated_heart_icon.dart';
-
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
@@ -12,110 +10,146 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final _emailController = TextEditingController();
-
   final _passwordController = TextEditingController();
 
   bool _obscurePassword = true;
-
+  
   @override
   Widget build(BuildContext context) {
     final colorScheme = AppColors.colorScheme;
-    return Scaffold(
-      backgroundColor: colorScheme.surface,
-      body: Padding(
-        padding: const EdgeInsets.all(32.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        backgroundColor: colorScheme.surface,
+        body: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const VerticalSpace(50),
 
-            /// main icon
-            // Padding(
-            //   padding: const EdgeInsets.only(top: 48),
-            //   child: Column(
-            //     mainAxisAlignment: MainAxisAlignment.center,
-            //     children: [
-            //       /// heart icon
-            //       const AnimatedHeartIcon(),
-            //     ],
-            //   ),
-            // ),
-
-            const VerticalSpace(75),
-
-            /// Login label and subtitle
-            Text(
-              'Login',
-              style: TextStyle(
-                color: colorScheme.primary,
-                fontSize: 50,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-
-            const VerticalSpace(6),
-
-            /// subtitle
-            Text(
-              'Please login to continue',
-              style: TextStyle(
-                color: colorScheme.secondary,
-                fontSize: 18,
-                // fontWeight: FontWeight.bold,
-              ),
-            ),
-
-            const VerticalSpace(100),
-
-            /// email text field
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                label: Text('Email'),
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.email),
-              ),
-            ),
-
-            const VerticalSpace(15),
-
-            /// password text field
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                label: Text('Password'),
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.lock),
-                suffixIcon: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      _obscurePassword = !_obscurePassword;
-                    });
-                  },
-
-                  child: TweenAnimationBuilder<double>(
-                    tween: Tween<double>(
-                      begin: 0,
-                      end: _obscurePassword ? 0 : 1,
-                    ),
-                    duration: Duration(milliseconds: 500),
-                    builder: (context, value, child) {
-                      return Transform.rotate(
-                        angle: value * 3.14,
-                        child: Icon(
-                          _obscurePassword ? Icons.visibility : Icons.visibility_off,
-                        ),
-                      );
-                    },
+                /// Login label and subtitle
+                Text(
+                  'Login',
+                  style: TextStyle(
+                    color: colorScheme.primary,
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-              obscureText: _obscurePassword,
+
+                const VerticalSpace(6),
+
+                /// subtitle
+                Text(
+                  'Please login to continue',
+                  style: TextStyle(
+                    color: colorScheme.secondary,
+                    fontSize: 18,
+                    // fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                const VerticalSpace(100),
+
+                /// email text field
+                TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    label: Text('Email'),
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.email),
+                  ),
+                ),
+
+                const VerticalSpace(15),
+
+                /// password text field
+                TextField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    label: Text('Password'),
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.lock),
+                    suffixIcon: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _obscurePassword = !_obscurePassword;
+                        });
+                      },
+
+                      child: TweenAnimationBuilder<double>(
+                        tween: Tween<double>(
+                          begin: 0,
+                          end: _obscurePassword ? 0 : 1,
+                        ),
+                        duration: Duration(milliseconds: 500),
+                        builder: (context, value, child) {
+                          return Transform.rotate(
+                            angle: value * 3.14,
+                            child: Icon(
+                              _obscurePassword
+                                  ? Icons.visibility_off
+                                  : Icons.visibility,
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                  obscureText: _obscurePassword,
+                ),
+
+                const VerticalSpace(30),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    /// Login button
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: colorScheme.surface,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      onPressed: login,
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          color: colorScheme.primary,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+
+                    /// Forgot password button/text
+                    TextButton(
+                      onPressed: forgotPassword,
+                      child: Text(
+                        'Forgot your password?',
+                        style: TextStyle(color: colorScheme.secondary),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
   }
+}
+
+void login() {
+  print('login');
+}
+
+void forgotPassword() {
+  print('forgot password');
 }
 
 class VerticalSpace extends StatelessWidget {
